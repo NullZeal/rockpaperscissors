921b0e09a9d82643258897de78868ffe901af082
nullzeal <nullzeal@pm.me>
2022-06-13 10:41:14 -0400

    Added testing functions for rock, paper and scissors stirngs

--- js/index.js
+++ js/index.js
@@ -1,30 +1,51 @@
-function getPlayerInput(){
-    let keepGoing = true;
-    while(keepGoing){
-        let playerInput = prompt('Select either Rock, Paper or Scissors by typing your answer.');
-        let tester = /^rock$|^paper$|^scissors$/i;
-        if (!tester.test(playerInput)){
-            alert('Wrong Answer!');
-        }
-        else{
-            keepGoing = false;
-        }
-    }
+getPlayerInputInString();
+
+function validateResult(getPlayerInputInString, getRandomRockPaperScissorsResultInString){
+    if (getPlayerInputInString == 'Rock') 
 
 }
-getPlayerInput()
 
 function resultOfOneGame(playerInput, aiResult){
 
 }
 
-function getRockPaperScissorsAIResult(){
-    let possibleAnswers = ["Rock", "Paper", "Scissors"];
-    let answer = possibleAnswers[Math.floor(Math.random() * (possibleAnswers.length - 0) + 0)].toString();
-    return answer;
-}
-
 // const resultOfAi = document.getElementById('computerPlayResult');
 // resultOfAi.insertAdjacentText('beforeend', getRockPaperScissorsAIResult().toString());
 
+function testRock(string){
+    let tester = /^rock$/i;
+    return tester.test(string)
+}
 
+function testRock(string){
+    let tester = /^paper$/i;
+    return tester.test(string)
+}
+
+function testRock(string){
+    let tester = /^scissors$/i;
+    return tester.test(string)
+}
+
+function getRandomRockPaperScissorsResultInString(){
+    let possibleAnswers = ["Rock", "Paper", "Scissors"];
+    let answer = possibleAnswers[Math.floor(Math.random() * (possibleAnswers.length - 0) + 0)];
+    return answer;
+}
+
+function getPlayerInputInString(){
+    let keepGoing = true;
+    let validatedPlayerInput = "";
+    while(keepGoing){
+        let unvalidatedPlayerInput = prompt('Select either Rock, Paper or Scissors by typing your answer.');
+        let tester = /^rock$|^paper$|^scissors$/i;
+        if (!tester.test(unvalidatedPlayerInput)){
+            alert('Wrong Answer!');
+        }
+        else{
+            keepGoing = false;
+            validatedPlayerInput = unvalidatedPlayerInput;
+        }
+    }
+    return validatedPlayerInput;
+}
