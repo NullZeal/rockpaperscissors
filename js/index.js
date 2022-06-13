d0b8d7a99d742a764e543ca0c9e911150fffa137
nullzeal <nullzeal@pm.me>
2022-06-13 10:13:28 -0400

    Setting up the html, css and js files

---
+++ js/index.js
@@ -0,0 +1,14 @@
+let playerInput = prompt('Select either Rock, Paper or Scissors by typing your answer.');
+
+function resultOfOneGame(playerInput, )
+function getRockPaperScissorsAIResult(){
+    let possibleAnswers = ["Rock", "Paper", "Scissors"];
+    let answer = possibleAnswers[Math.floor(Math.random() * (possibleAnswers.length - 0) + 0)].toString();
+
+    return answer;
+}
+
+// const resultOfAi = document.getElementById('computerPlayResult');
+// resultOfAi.insertAdjacentText('beforeend', getRockPaperScissorsAIResult().toString());
+
+
