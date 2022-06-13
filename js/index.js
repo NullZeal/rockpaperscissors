3b3f28d7e4310a83566703e99835912187d18ce9
nullzeal <nullzeal@pm.me>
2022-06-13 10:25:28 -0400

    Added a regex tester function for player input

--- js/index.js
+++ js/index.js
@@ -1,10 +1,26 @@
-let playerInput = prompt('Select either Rock, Paper or Scissors by typing your answer.');
+function getPlayerInput(){
+    let keepGoing = true;
+    while(keepGoing){
+        let playerInput = prompt('Select either Rock, Paper or Scissors by typing your answer.');
+        let tester = /^rock$|^paper$|^scissors$/i;
+        if (!tester.test(playerInput)){
+            alert('Wrong Answer!');
+        }
+        else{
+            keepGoing = false;
+        }
+    }
 
-function resultOfOneGame(playerInput, )
+}
+getPlayerInput()
+
+function resultOfOneGame(playerInput, aiResult){
+
+}
+
 function getRockPaperScissorsAIResult(){
     let possibleAnswers = ["Rock", "Paper", "Scissors"];
     let answer = possibleAnswers[Math.floor(Math.random() * (possibleAnswers.length - 0) + 0)].toString();
-
     return answer;
 }
 
